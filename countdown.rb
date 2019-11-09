4ff5885f0cd720ed607fdb7e4f4293fc68ad06db
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-09 23:43:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,12 @@
 #write your code here
-
+countdown = 0
 def countdown(number)
   while countdown < 10
   countdown -= 1
   puts "#{number} SECOND(S)!"
 end
 end
-puts countdown
+
 
 
 def countdown_with_sleep
