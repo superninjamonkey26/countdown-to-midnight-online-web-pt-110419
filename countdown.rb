dee5da3244b076973ba190e18ba10d5b7674a8e4
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-09 23:58:48 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
-#write your code here
+def countdown(seconds)
+  while seconds > 0
+  puts "#{seconds} SECOND(S)!"
+  seconds -= 1
+end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(seconds)
+  sleep(5)
 end
